86e58d0bce8b1d0d0e1301a712830e41558e0eba
hamza-bunya <nawaz_hamzaa@yahoo.com>
2022-11-07 08:02:54 +0500

    Changed backend

--- utils/api.js
+++ utils/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // This is the main HTTP client for the application. Creating a unified client 
 // will ease global changes like headers & baseURLs etc.
 const api = axios.create({
-    baseURL: 'http://creable-001-site1.atempurl.com/',
+    baseURL: 'https://creable.azurewebsites.net/',
     headers: {
         'Access-Control-Allow-Origin' : '*'
     }
